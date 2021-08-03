6e517db305037454c6908f1392f4534bfa84bba3
nsilveri <nikis900@gmail.com>
2021-08-03 03:38:55 +0200

    Update Battery_LED_monitor_installer.sh

--- Battery_monitor/Battery_LED_monitor_installer.sh
+++ Battery_monitor/Battery_LED_monitor_installer.sh
@@ -1,7 +1,45 @@
 #!/bin/sh
-wget get-pip.py
+
+#Enable I2C
+echo '>>> Enable I2C'
+if grep -q 'i2c-bcm2708' /etc/modules; then
+  echo 'Seems i2c-bcm2708 module already exists, skip this step.'
+else
+  echo 'i2c-bcm2708' >> /etc/modules
+fi
+if grep -q 'i2c-dev' /etc/modules; then
+  echo 'Seems i2c-dev module already exists, skip this step.'
+else
+  echo 'i2c-dev' >> /etc/modules
+fi
+if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
+  echo 'Seems i2c1 parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c1=on' >> /boot/config.txt
+fi
+if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
+  echo 'Seems i2c_arm parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+fi
+if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+  sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+  sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+else
+  echo 'File raspi-blacklist.conf does not exist, skip this step.'
+fi
+#Install Python PIP
+echo '>>> Download get-pip.py'
+wget https://bootstrap.pypa.io/get-pip.py
+echo '>>> Install Python PIP'
+python get-pip.py
+echo '>>> Install ads1115 python library'
 pip install ads1115
+echo '>>> Save Batocera Overlay'
 bash batocera-save-overlay
-wget battery_monitor.py
+
+#Setting Script Startup
+echo '>>> Autoboot script...'
 mv custom.sh /userdata/system
 bash batocera-save-overlay
+exit '0'
