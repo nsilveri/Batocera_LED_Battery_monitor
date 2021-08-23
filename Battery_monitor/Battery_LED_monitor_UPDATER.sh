9a19ecf2f873ce1fda25537b9721d06d59a75ab1
nsilveri <nikis900@gmail.com>
2021-08-23 14:21:13 +0200

    Update Battery_LED_monitor_UPDATER.sh

--- Battery_monitor/Battery_LED_monitor_UPDATER.sh
+++ Battery_monitor/Battery_LED_monitor_UPDATER.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-FILE_1=/userdata/Battery_monitor/Battery_LED_monitor_TinkerBoy_installer.sh
-FILE_2=/userdata/Battery_monitor/Battery_LED_monitor_installer.sh
-if test -f "$FILE_1"; then
+#FILE_1=/userdata/Battery_monitor/Battery_LED_monitor_TinkerBoy_installer.sh
+#FILE_2=/userdata/Battery_monitor/Battery_LED_monitor_installer.sh
+if test -f "/userdata/Battery_monitor/Battery_LED_monitor_TinkerBoy_installer.sh"; then
     echo "$FILE_1 old script found."
-	rm $FILE_1
+	rm Battery_LED_monitor_TinkerBoy_installer.sh
 	echo "Downloading last "LED_monitor_TinkerBoy_installer" script version..."
 	wget https://raw.githubusercontent.com/nsilveri/Batocera_LED_Battery_monitor/main/Battery_monitor/Battery_LED_monitor_TinkerBoy_installer.sh
+else
+  wget https://raw.githubusercontent.com/nsilveri/Batocera_LED_Battery_monitor/main/Battery_monitor/Battery_LED_monitor_TinkerBoy_installer.sh
 fi
 
-if test -f "$FILE_2"; then
-    echo "$FILE_2 old script found."
-	rm $FILE_2
+
+if test -f "Battery_LED_monitor_installer.sh"; then
+    echo "Battery_LED_monitor_installer.sh old script found."
+	rm Battery_LED_monitor_installer.sh
 	echo "Downloading last "LED_monitor_installer" script version..."
 	wget https://raw.githubusercontent.com/nsilveri/Batocera_LED_Battery_monitor/main/Battery_monitor/Battery_LED_monitor_installer.sh
+else
+  wget https://raw.githubusercontent.com/nsilveri/Batocera_LED_Battery_monitor/main/Battery_monitor/Battery_LED_monitor_installer.sh
 fi
 
-bash $FILE_2
+bash Battery_LED_monitor_installer.sh
